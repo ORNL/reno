d43236a3467736aee7b666e66d4487065ea48c30
Martindale, Nathan <martindalena@ornl.gov>
2026-03-18 11:34:53 -0400

    Clean init.py

--- reno/__init__.py
+++ reno/__init__.py
@@ -5,43 +5,54 @@ System dynamics library
 Library for constructing, debugging, visualizing, and analyzing system
 dynamics models and simulations.
 
->>> from reno.components import Stock, Flow, Variable, Scalar, TimeRef
->>> from reno.model import Model
->>> from reno import ops
->>> t = TimeRef()
->>> tub = Model("tub")
->>> tub.faucet = Flow(Scalar(5))
->>> tub.drain = Flow(ops.sin(t) + 2)
->>> tub.water_level = Stock()
->>> tub.water_level += tub.faucet
->>> tub.water_level -= tub.drain
+>>> import reno as r
+>>> t = r.TimeRef()
+>>> tub = r.Model("tub")
+>>> with tub:
+>>>     faucet = r.Flow(r.Scalar(5))
+>>>     drain = r.Flow(r..sin(t) + 2)
+>>>     water_level = r.Stock()
+>>>     faucet >> water_level >> drain
 >>> tub()
-"""
+"""  # noqa: D205, D212, D415
 
 import warnings
 
-import reno.components  # noqa: F401
-import reno.diagrams  # noqa: F402
-import reno.explorer  # noqa: F401
-import reno.model  # noqa: F401
-import reno.ops  # noqa: F401
-import reno.pymc  # noqa: F401
-import reno.viz  # noqa: F401
-from reno.components import Flag  # noqa: F401
-from reno.components import Flow  # noqa: F401
-from reno.components import Function  # noqa: F401
-from reno.components import Metric  # noqa: F401
-from reno.components import Piecewise  # noqa: F401
-from reno.components import Scalar  # noqa: F401
-from reno.components import Stock  # noqa: F401
-from reno.components import TimeRef  # noqa: F401
-from reno.components import Variable  # noqa: F401
-from reno.model import Model  # noqa: F401
-from reno.ops import *  # noqa: F401, F403
-from reno.viz import plot_refs  # noqa: F401
-from reno.viz import plot_refs_single_axis  # noqa: F401
-from reno.viz import plot_trace_refs  # noqa: F401
+from reno.components import (
+    Flag,
+    Flow,
+    Function,
+    Metric,
+    Piecewise,
+    Scalar,
+    Stock,
+    TimeRef,
+    Variable,
+)
+from reno.model import Model
+from reno.ops import *  # noqa: F403
+from reno.viz import (
+    plot_refs,
+    plot_refs_single_axis,
+    plot_trace_refs,
+)
 
 warnings.simplefilter("always", RuntimeWarning)
 
 __version__ = "0.12.0"
+
+__all__ = [
+    "Flag",
+    "Flow",
+    "Function",
+    "Metric",
+    "Model",
+    "Piecewise",
+    "Scalar",
+    "Stock",
+    "TimeRef",
+    "Variable",
+    "plot_refs",
+    "plot_refs_single_axis",
+    "plot_trace_refs",
+]
