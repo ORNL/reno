Fix problem with broadcasting vs padding in initial values

reno/components.py
@@ -1927,7 +1927,11 @@ class TrackedReference(Reference):
             # (e.g. Variable(5, dim=6) needs to return [5, 5, 5, 5, 5, 5] both
             # times)
             if self.dim > 1:
-                val = np.broadcast_to(val, (self.dim,))
+                if not isinstance(val, np.ndarray):
+                    val = np.broadcast_to(val, (self.dim,))
+                else:
+                    val = np.pad(val, (0, self.dim - val.shape[0]))
+
                 # if not isinstance(val, np.ndarray):
                 #     val = np.array([val])
                 # if val.shape[-1] == 1:
@@ -2330,6 +2334,19 @@ class Flow(TrackedReference):
                 init_eq = self._implied_eq()
             resolved_init_value = self.resolve_init_array(init_eq)
 
+            if self.dim > 1:
+                # TODO: make sure this logic is replicated into flow as
+                # well?
+                if not isinstance(resolved_init_value, np.ndarray):
+                    resolved_init_value = np.broadcast_to(
+                        resolved_init_value, (self.dim,)
+                    )
+                else:
+                    resolved_init_value = np.pad(
+                        resolved_init_value,
+                        (0, self.dim - resolved_init_value.shape[0]),
+                    )
+
             # value shape cases for value assignment
             if self._static:
                 # case 1, simplest, raw value or (dim,)
@@ -2343,11 +2360,10 @@ class Flow(TrackedReference):
                 # in the correctly repeated value at that point? We shouldn't be
                 # responsible for that and it's a little weird.
                 # ensure the value matches a manually specified dimensionality
-                if self.dim > 1:
-                    # ensure
-                    resolved_init_value = np.broadcast_to(
-                        resolved_init_value, (self.dim,)
-                    )
+                # # ensure
+                # resolved_init_value = np.broadcast_to(
+                #     resolved_init_value, (self.dim,)
+                # )
 
                 # ensure correct type (only needed in this case, other cases
                 # don't re-assign container value)
@@ -2520,12 +2536,25 @@ class Variable(TrackedReference):
                 init_eq = self._implied_eq()
             resolved_init_value = self.resolve_init_array(init_eq)
 
-            # value shape cases for value assignment
-            if self._static:
-                if self.dim > 1:
+            if self.dim > 1:
+                # TODO: make sure this logic is replicated into flow as
+                # well?
+                if not isinstance(resolved_init_value, np.ndarray):
                     resolved_init_value = np.broadcast_to(
                         resolved_init_value, (self.dim,)
                     )
+                else:
+                    resolved_init_value = np.pad(
+                        resolved_init_value,
+                        (0, self.dim - resolved_init_value.shape[0]),
+                    )
+
+            # value shape cases for value assignment
+            if self._static:
+                # if self.dim > 1:
+                #     resolved_init_value = np.broadcast_to(
+                #         resolved_init_value, (self.dim,)
+                #     )
 
                 # ensure correct type (only needed in this case, other cases
                 # don't re-assign container value)

reno/ops.py
@@ -642,17 +642,25 @@ class orient_timeseries(reno.components.Operation):
         return True
 
     def op_eval(self, t: int, **kwargs: dict) -> np.ndarray:
-        full_steps = self.sub_equation_parts[0].model.steps
+        # if self.sub_equation_parts[0].model is not None:
+        #     full_steps = self.sub_equation_parts[0].model.steps
+        # elif self.sub_equation_parts[0].value is not None and reno.utils.shapiness(self.sub_equation_parts[0].value) > 0 and not self.sub_equation_parts[0].is_static():
+        #     full_steps = len(self.sub_equation_parts[0].value)
+        # else:
+        #     full_steps = 10
+        #
+
         values = []
-        for i in range(t):
+        for i in range(t + 1):
             values.append(self.sub_equation_parts[0].eval(i, **kwargs))
 
-        remainder = full_steps - t
+        # remainder = full_steps - (t + 1)
         # NOTE: ...this is the dumbest way possible to ensure type is kept...but
-        # it works?
+        # it works? I'm going to claim that with horseshoe theory, this is so
+        # dumb that it's actually smart.
         zero_value = values[0] * 0
-        for i in range(remainder):
-            values.append(zero_value)
+        # for i in range(remainder):
+        #     values.append(zero_value)
 
         return np.array(values)
 